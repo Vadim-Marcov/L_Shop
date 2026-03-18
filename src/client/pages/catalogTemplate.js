d769836ba8fd0b1c0a12c0ec2a245e3f0164f65c
qshendal <dimaadinec@gmail.com>
2026-03-18 09:29:54 +0300

    Созданы шаблоны для витрины товаров и корзины

---
+++ src/client/pages/catalogTemplate.js
@@ -0,0 +1,10 @@
+export const catalogHTML = `
+    <h2>Каталог товаров</h2>
+    <div class="product-grid" id="products-container">
+        <div class="product-card" data-product-id="1">
+            <h3 data-title>Худи L_Shop</h3>
+            <p data-price>5000 руб.</p>
+            <button class="btn" data-add-to-cart>В корзину</button>
+        </div>
+    </div>
+`;
